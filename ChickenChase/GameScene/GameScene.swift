ef8f99e6211828cc78893c173b23c442d6147364
Ryan Lauer <ryandlauer@gmail.com>
2020-06-22 16:03:37 -0500

    structure layout

---
+++ ChickenChase/GameScene/GameScene.swift
@@ -0,0 +1,30 @@
+//
+//  GameScene.swift
+//  ChickenChase
+//
+//  Created by Ryan Lauer on 6/22/20.
+//  Copyright © 2020 LauTech. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func sceneDidLoad() {
+
+       
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+       
+    }
+}
---
+++ ChickenChase/LoadingScene/LoadingScene.swift
@@ -0,0 +1,9 @@
+//
+//  LoadingScene.swift
+//  ChickenChase
+//
+//  Created by Ryan Lauer on 6/22/20.
+//  Copyright © 2020 LauTech. All rights reserved.
+//
+
+import Foundation
